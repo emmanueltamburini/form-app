88ae757c84594f2b58a166a0e69366cb062419a9
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-20 21:57:59 -0400

    template dynamics form has been added

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {
+  name: string,
+  favorites: Favorite[]
+}
+
+interface Favorite {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html'
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
 
-  constructor() { }
+  public newFavorite: string = '';
 
-  ngOnInit(): void {
+  public person: Person = {
+    name: 'Emmanuel',
+    favorites: [
+      {id: 1, name: 'Galletas'},
+      {id: 2, name: 'Helado'}
+    ]
   }
 
+  public onSubmit(): void {
+
+  }
+
+  public onAdd(): void {
+    const newFavorite: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newFavorite
+    }
+    this.person.favorites.push({...newFavorite});
+    this.newFavorite = '';
+  }
+
+  public onDelete(index: number): void {
+    this.person.favorites.splice(index, 1);
+  }
 }
