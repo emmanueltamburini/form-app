15b38fd193a969412a07cdb106d44a37bc5b0775
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-27 22:00:43 -0400

    login form has been added

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/shared/validator/login.service';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  public form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required,/* Validators.email*/Validators.pattern(this.validatorService.emailPattern)], [this.loginService]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {
+  get errorEmailMessage(): string {
+    const errors = this.form.get('email')?.errors;
+
+    if (errors && errors['required']) {
+      return 'Email is required';
+    } else if (errors && errors['pattern']) {
+      return 'Email must be in format';
+    } else if (errors && errors['emailNoRegister']) {
+      return 'Email is not register';
+    }
+
+    return ''
   }
 
+  get errorPasswordMessage(): string {
+    const errors = this.form.get('password')?.errors;
+
+    if (errors && errors['required']) {
+      return 'Password is required';
+    } else if (errors && errors['minlength']) {
+      return `Password must be greater than ${errors['minlength'].requiredLength}`;
+    }
+
+    return ''
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private validatorService: ValidatorService,
+    private loginService: LoginService
+  ) { }
+
+  public fieldNoValid(field: string): boolean {
+    return !!this.form.get(field)?.invalid && !!this.form.get(field)?.touched;
+  }
+
+  public onSubmit(): void {
+    console.log(this.form.value);
+    this.form.markAllAsTouched();
+  }
 }
---
+++ src/app/shared/validator/login.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { delay, map, Observable } from 'rxjs';
+import { User } from '../interfaces/user.interfaces';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService implements AsyncValidator {
+
+  private _baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+    const params: HttpParams = new HttpParams()
+      .set('q', control.value);
+
+    return this.http.get<User[]>(`${this._baseUrl}/users`, {params})
+    .pipe(
+      delay(1000),
+      map(response => (response.length !== 0) ? null : {emailNoRegister: true})
+    );
+  }
+}
