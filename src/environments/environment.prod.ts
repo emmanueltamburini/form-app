b64ea6d57ef945e0d1c914a5066e4a729b5aefba
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-29 17:28:32 -0400

    prod credentials has been added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://healthy-evanescent-ferry.glitch.me',
+  countryBaseUrl: 'https://restcountries.com/v2',
 };
